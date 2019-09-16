c9d54da3acfa0761c644147d4f121153e233721f
prakash karena <prakash.raoinfotech@gmail.com>
2019-09-16 16:49:04 +0530

    add build commit

---
+++ client/build/precache-manifest.2e823895921ca29ec0d348df89241300.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9bdfd35e4710a30ea615e89351695f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4ccc64b3e0387dfbb3",
+    "url": "/static/js/2.28322bd3.chunk.js"
+  },
+  {
+    "revision": "42533f3146e93ec58469",
+    "url": "/static/js/main.eaa87165.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
